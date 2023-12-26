8be80c8b4ed26aa9873e6d6cb8f9836dc7062cdf
Enrico <enrico.voltan@mail.polimi.it>
2023-12-26 09:12:16 +0100

    Client Error + Log

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,16 @@
 use std::fmt::Display;
 
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{
+    http::{response, StatusCode},
+    response::IntoResponse,
+};
+use serde::Serialize;
+use serde_with::SerializeDisplay;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
 
@@ -17,10 +23,50 @@ pub enum Error {
     TicketDeleteFailNotFound { id: u64 },
 }
 
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Error::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // -- Auth
+            Error::AuthFailNoAuthTokenCookie
+            | Error::AuthFailTokenWrongFormat
+            | Error::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // -- Model
+            Error::TicketDeleteFailNotFound { id } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // -- Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
 impl IntoResponse for Error {
     fn into_response(self) -> axum::response::Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+
+        // Create a placeholder Acum response
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        response.extensions_mut().insert(self);
+
+        response
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 
 mod ctx;
 mod error;
+mod log;
 mod model;
 mod web;
 
+use crate::log::log_request;
 use crate::model::ModelController;
 
 pub use self::error::{Error, Result};
@@ -12,12 +14,16 @@ pub use self::error::{Error, Result};
 use std::os::unix::net::SocketAddr;
 
 use axum::extract::{Path, Query};
+use axum::http::{Method, Uri};
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::{middleware, Router};
+use axum::{middleware, Json, Router};
+use ctx::Ctx;
 use serde::Deserialize;
+use serde_json::json;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -48,10 +54,38 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn main_response_mapper(res: Response) -> Response {
+async fn main_response_mapper(
+    ctx: Option<Ctx>,
+    uri: Uri,
+    req_method: Method,
+    res: Response,
+) -> Response {
     println!("->> {:<12} - mapper", "RES_MAPPER");
-    println!();
-    res
+    let uuid = Uuid::new_v4();
+
+    // ll Get eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // -- if we have a client error, build response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid":uuid.to_string(),
+                }
+            });
+            println!("      ->> client_error_body: {client_error_body}");
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // -- Buil and log the server log line
+    let client_error = client_status_error.unzip().1;
+    log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
+
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
